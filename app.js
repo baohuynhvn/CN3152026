2d0d68a8e3f003bb7314d2d942efa9894ad221d4
baohuynhvn <toilacontrai73@gmail.com>
2026-03-15 14:53:04 +0700

    first commit

--- app.js
+++ app.js
@@ -1,55 +1,58 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-let mongoose = require('mongoose')
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+let mongoose = require("mongoose");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/api/v1/users', require('./routes/users'));
-app.use('/api/v1/products', require('./routes/products'));
-app.use('/api/v1/categories', require('./routes/categories'));
-app.use('/api/v1/roles', require('./routes/roles'));
-app.use('/api/v1/auth', require('./routes/auth'));
+app.use("/", indexRouter);
+app.use("/api/v1/users", require("./routes/users"));
+app.use("/api/v1/products", require("./routes/products"));
+app.use("/api/v1/categories", require("./routes/categories"));
+app.use("/api/v1/roles", require("./routes/roles"));
+app.use("/api/v1/auth", require("./routes/auth"));
 
-mongoose.connect('mongodb://localhost:27017/NNPTUD-C3');
-mongoose.connection.on('connected',()=>{
-  console.log("connected");
-})
-mongoose.connection.on('disconnected',()=>{
-  console.log("disconnected");
-})
+mongoose.connect(
+  "mongodb+srv://huynhgiabao280224_db_user:ZfOXRKXektTXlSq1@cluster0.rsvi5k9.mongodb.net/NNPTUD-C3?retryWrites=true&w=majority",
+);
 
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB Atlas connected");
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB Atlas disconnected");
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
